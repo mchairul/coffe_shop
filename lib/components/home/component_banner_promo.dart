7701dcd09ba8c84c4685e3a5d2957aa7a274500a
Muhammad Chairul <email.muhammad.chairul@gmail.com>
2024-12-06 15:21:44 +0700

    Update component_banner_promo.dart

--- lib/components/home/component_banner_promo.dart
+++ lib/components/home/component_banner_promo.dart
@@ -11,13 +11,79 @@ class ComponentBannerPromo extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: 200,
         decoration: BoxDecoration(
-          image: DecorationImage(
+          image: const DecorationImage(
               image: AssetImage("assets/images/banner-promo.png"),
               fit: BoxFit.cover
           ),
           borderRadius: BorderRadius.circular(30)
         ),
-        child: Text('ini'),
+        child: Padding(
+          padding: EdgeInsets.only(
+              left: 20,
+              top: 20
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xffED5151)
+                  ),
+                  child: Text('Promo', style: TextStyle(color: Colors.white),)
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Stack(
+                  alignment: AlignmentDirectional.topEnd,
+                  children: [
+                    Container(
+                        width: 200,
+                        height:30,
+                        decoration: BoxDecoration(
+                            color: Color(0xff111111)
+                        ),
+                        child: null
+                    ),
+                    Positioned(
+                      child: Text('Buy one get',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Stack(
+                  alignment: AlignmentDirectional.topEnd,
+                  children: [
+                    Container(
+                        width: 170,
+                        height:30,
+                        decoration: BoxDecoration(
+                            color: Color(0xff111111)
+                        ),
+                        child: null
+                    ),
+                    Positioned(
+                      child: Text('one FREE',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
